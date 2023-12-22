454647bc5cc0090fe05eb1f9682565ba82f38d3c
waelkamira <ramondm4@gmail.com>
2023-12-22 18:55:30 +0300

    add body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,20 @@
 import HomeHeader from "@/components/HomeHeader";
-import Link from "next/link";
-import { TbGridDots } from "react-icons/tb";
-
+import Image from "next/image";
+import HomeSearch from "@/components/HomeSearch";
 export default function Home(props) {
   return (
-    <header className="flex justify-end p-5 text-sm">
-      <div className="flex space-x-4 items-center">
-        <Link href="https://mail.google.com" className="hover:underline">
-          Gmail
-        </Link>
-        <Link href="https://image.google.com" className="hover:underline">
-          Images
-        </Link>
-        <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2" />
-        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">
-          Sign In
-        </button>
+    <>
+      {/* header */}
+      <HomeHeader />
+      {/* body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width={300}
+          height={100}
+          src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg"
+        ></Image>
+        <HomeSearch />
       </div>
-    </header>
+    </>
   );
 }
---
+++ src/components/HomeSearch.jsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState } from "react";
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMicrophone } from "react-icons/fa";
+import { useRouter } from "next/navigation";
+export default function HomeSearch() {
+  const [input, setInput] = useState("");
+  const router = useRouter();
+  const [randomSearchLoading, setRandomSearchLoading] = useState(false);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search/web?searchTerm=${input}`);
+  }
+  async function randomSearch() {
+    setRandomSearchLoading(true);
+    const response = await fetch("https://random-word-api.herokuapp.com/word")
+      .then((res) => res.json())
+      .then((data) => data[0]);
+
+    if (!response) return;
+    router.push(`/search/web?searchTerm=${response}`);
+    setRandomSearchLoading(false);
+  }
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full mt-5 :mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+      >
+        <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          className="flex-grow focus:outline-none "
+        />
+        <FaMicrophone className="text-lg" />
+      </form>
+      <div
+        className="flex flex-col space-y-2 sm:space-y-0
+      sm:space-x-4 justify-center sm:flex-row mt-8"
+      >
+        <button onClick={handleSubmit} className="btn">
+          Google Search
+        </button>
+        <button
+          disabled={randomSearchLoading}
+          onClick={randomSearch}
+          className="btn flex items-center justify-center disabled:opacity-90"
+        >
+          {randomSearchLoading ? (
+            <img
+              src="spinner.svg"
+              alt="Loading ..."
+              className="h-6 text-center"
+            />
+          ) : (
+            "I'm Feeling Lucky"
+          )}
+        </button>
+      </div>
+    </>
+  );
+}
