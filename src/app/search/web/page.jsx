d36c5931c3054a05c3ef0854e3e20fd440e226b9
waelkamira <ramondm4@gmail.com>
2023-12-26 15:09:39 +0300

    fetch data from google search API

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { RiH1 } from "react-icons/ri";
 
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  console.log(data);
+  const results = data.items;
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
